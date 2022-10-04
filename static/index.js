431f861273de6591e23e927d00690ab3502bff17
Kardna <76856768+ZenfyDev@users.noreply.github.com>
2022-10-04 16:21:11 -0400

    Kardna

--- static/index.js
+++ static/index.js
@@ -1,5 +1,3 @@
-var urlObj = new window.URL(window.location.href); var url = "https://kardna-v2.herokuapp.com/games%20page.html"; if (url) { var win; document.querySelector(Games).onclick = function() { if (win) { win.focus(); } else { win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var iframe = win.document.createElement('iframe'); iframe.style.border = 'none'; iframe.style.width = '100%'; iframe.style.height = '100%'; iframe.style.margin = '0'; iframe.src = url; win.document.body.appendChild(iframe); } document.querySelector(Games).style.background='#00000'; document.querySelector(Games).innerHTML="Page Opened!"; }; } 
-
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const queryString = window.location.search;
@@ -25,38 +23,3 @@ function isUrl(val = ''){
     return false;
 };
 
-function settings() {
-	var win = window.open("/settings.html", null, "height=500,width=500,status=yes,toolbar=no,menubar=no,location=no");
-	win.moveTo(screen.width / 3, screen.height / 3);
-}
-
-function sboxPopup() {
-	var sbox = document.querySelector(".sBox");
-	var input = document.querySelector(".sBox input");
-
-	document.onkeypress = function(e) {
-		if (e.key == "Enter") sbox.style.display = "none";
-	};
-
-	var display = sbox.style.display == "inline";
-	if (display) {
-		display = "none";
-		sbox.style = `position: fixed;
-	animation-name: sboxslideout;
-  animation-duration: 2s;`;
-	} else {
-		display = "inline-block";
-		sbox.style = `position: fixed;
-	top: 6.6%;
-	bottom: 0;
-	left: 80%;
-	z-index: 99;
-	animation-name: sboxslidein;
-  animation-duration: 2s;
-	color: #000056;
-	border: none;
-	width: 10%;
-	height: 40px;`
-	}
-	sbox.style.display = display;
-}
