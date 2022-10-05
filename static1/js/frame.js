a5291f9e4c2412de7bf1ca767e7e05989c4c3bc5
Kardna <76856768+ZenfyDev@users.noreply.github.com>
2022-10-05 08:00:56 -0400

    Mine

---
+++ static1/js/frame.js
@@ -0,0 +1,15 @@
+function h()
+{
+    var src = document.getElementById("mainframe");
+    src.src = './home.html';
+}
+function m()
+{
+    var src = document.getElementById("mainframe");
+    src.src = './minecraft.html';
+}
+function g()
+{
+    var src = document.getElementById("mainframe");
+    src.src = './game.html';
+}
